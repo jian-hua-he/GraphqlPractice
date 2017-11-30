f81dd57968eac5fa211c5f2ee865835e9c528dd3
Jian-Hua, He <game09116@gmail.com>
2017-11-30 23:23:42 +0800

    Add basic schema

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import graphqlHttp from 'express-graphql';
+
+import schema from './graphql/schema';
 
 export default {
     init: () => {
         const app = express();
 
         app.get('/', (req, res) => res.send('Hello World!'));
+
+        app.use('/graphql', graphqlHttp({
+            schema: schema,
+            graphiql: true,
+        }));
 
         app.listen(5000, () => console.log('Example app listening on port 5000!'));
     },
