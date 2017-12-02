61a28ecf17d6341e59bf1e10d3846c3300414eb2
Jian-Hua, He <game09116@gmail.com>
2017-12-02 13:59:03 +0800

    Extract fakeData to data.json

--- src/graphql/todo/query.js
+++ src/graphql/todo/query.js
@@ -4,21 +4,7 @@ import {
 } from 'graphql';
 
 import todoType from './type';
-
-let fakeData = [
-    {
-        id: 1,
-        title: 'Clean House',
-        description: 'This house is too dirty. Should clean it up',
-        checked: true,
-    },
-    {
-        id: 1,
-        title: 'Buy some food',
-        description: 'We don’t have any food in our storage. Go to market and buy some',
-        checked: false,
-    },
-];
+import fakeData from './data.json';
 
 export let todos = {
     type: new GraphQLList(todoType),
