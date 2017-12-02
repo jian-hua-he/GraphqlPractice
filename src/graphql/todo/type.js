8133d0991b473dcaced07072f4d5da5c8b300d1e
Jian-Hua, He <game09116@gmail.com>
2017-12-02 15:10:28 +0800

    Add user relation in todo type

--- src/graphql/todo/type.js
+++ src/graphql/todo/type.js
@@ -5,6 +5,9 @@ import {
     GraphQLObjectType
 } from 'graphql';
 
+import userType from '../user/type';
+import userList from '../../db/fake/user.json';
+
 let todoType = new GraphQLObjectType({
     name: 'Todo',
     description: 'Todo graphql type',
@@ -24,6 +27,15 @@ let todoType = new GraphQLObjectType({
         checked: {
             description: 'Check todo is completed',
             type: GraphQLBoolean,
+        },
+        user: {
+            description: 'Owner data',
+            type: userType,
+            resolve: function(todo, args, info) {
+                return userList.find((user) => {
+                    return user.id == todo.userId;
+                });
+            },
         },
     },
 });
