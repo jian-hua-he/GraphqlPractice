73fd7976b8dad2f8f5ec54d7c1ec5d3bae188f65
Jian-Hua, He <game09116@gmail.com>
2017-12-02 15:50:17 +0800

    Add todo field

--- src/graphql/user/type.js
+++ src/graphql/user/type.js
@@ -1,25 +1,41 @@
 import {
     GraphQLID,
     GraphQLString,
+    GraphQLList,
     GraphQLObjectType
 } from 'graphql';
+
+import todoList from '../../db/fake/todo.json';
 
 const userType = new GraphQLObjectType({
     name: 'User',
     description: 'User graphql Type',
-    fields: {
-        id: {
-            description: 'User’s unique id',
-            type: GraphQLID,
-        },
-        name: {
-            description: 'User’s name',
-            type: GraphQLString,
-        },
-        email: {
-            description: 'User’s email',
-            type: GraphQLString,
-        },
+    fields: () => {
+        const todoType = require('../todo/type').default;
+
+        return {
+            id: {
+                description: 'User’s unique id',
+                type: GraphQLID,
+            },
+            name: {
+                description: 'User’s name',
+                type: GraphQLString,
+            },
+            email: {
+                description: 'User’s email',
+                type: GraphQLString,
+            },
+            todos: {
+                description: 'User’s todo list',
+                type: new GraphQLList(todoType),
+                resolve: function(user, args, info) {
+                    return todoList.filter((todo) => {
+                        return todo.userId == user.id;
+                    });
+                },
+            },
+        };
     },
 });
 
