5fec998eb695f876e82959b2414dd57a6fa41b2f
Jian-Hua, He <game09116@gmail.com>
2017-12-02 15:52:04 +0800

    Fix circular dependencies

--- src/graphql/todo/type.js
+++ src/graphql/todo/type.js
@@ -5,39 +5,42 @@ import {
     GraphQLObjectType
 } from 'graphql';
 
-import userType from '../user/type';
 import userList from '../../db/fake/user.json';
 
 const todoType = new GraphQLObjectType({
     name: 'Todo',
     description: 'Todo graphql type',
-    fields: () => ({
-        id: {
-            description: 'Todo’s unique id',
-            type: GraphQLID,
-        },
-        title: {
-            description: 'Todo’s name',
-            type: GraphQLString,
-        },
-        description: {
-            description: 'Todo’s description',
-            type: GraphQLString,
-        },
-        checked: {
-            description: 'Check todo is completed',
-            type: GraphQLBoolean,
-        },
-        user: {
-            description: 'Owner data',
-            type: userType,
-            resolve: function(todo, args, info) {
-                return userList.find((user) => {
-                    return user.id == todo.userId;
-                });
+    fields: () => {
+        let userType = require('../user/type').default;
+
+        return {
+            id: {
+                description: 'Todo’s unique id',
+                type: GraphQLID,
             },
-        },
-    }),
+            title: {
+                description: 'Todo’s name',
+                type: GraphQLString,
+            },
+            description: {
+                description: 'Todo’s description',
+                type: GraphQLString,
+            },
+            checked: {
+                description: 'Check todo is completed',
+                type: GraphQLBoolean,
+            },
+            user: {
+                description: 'Owner data',
+                type: userType,
+                resolve: function(todo, args, info) {
+                    return userList.find((user) => {
+                        return user.id == todo.userId;
+                    });
+                },
+            },
+        };
+    },
 });
 
 export default todoType;
